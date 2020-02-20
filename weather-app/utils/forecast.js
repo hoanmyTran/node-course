b5d43beecff6d4c3b3e04ee8e16c3f6a4366db09
hoan-my.tran <hoan-my.tran@horoquartz.fr>
2020-02-20 15:31:32 +0100

    shorthand and destructuring

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 const forecast = (latitude, longtitude, callback) => {
     const url = 'https://api.darksky.net/forecast/2ed585c49de5548d374e5316b28fcfb1/'+latitude+','+longtitude+'?units=us';
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, response.body.daily.data[0].summary+ ' It is currently '+response.body.currently.temperature+' degress out. There is a '+ response.body.currently.precipProbability+ ' chance of rain');
+            callback(undefined, body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degress out. There is a '+ body.currently.precipProbability+ ' chance of rain');
         }
     });
 }
