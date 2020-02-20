97b28941ce5a2a5aa056f2d83839521f3a1ed418
hoan-my.tran <hoan-my.tran@horoquartz.fr>
2020-02-20 08:50:41 +0100

    readNote done

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,14 +1,11 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = function() {
-    return 'Your notes...';
-}
 const addNote = (title, body) => {
     const notes = loadNotes();
-    const duplicateNotes = notes.filter(note => note.title === title);
+    const duplicateNote = notes.find(note => note.title === title);
 
-    if(duplicateNotes.length == 0) {
+    if(!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -41,6 +38,17 @@ const listNotes = () => {
     });
 }
 
+const readNote = title => {
+    const notes = loadNotes();
+    note = notes.find(note => note.title === title);
+    if(note) {
+        console.log(chalk.inverse(note.title));
+        console.log(note.body);
+    } else {
+        console.log(chalk.red.inverse(('Note not found')));
+    }
+}
+
 const saveNotes = notes => {
     const dataJSON = JSON.stringify(notes);
     fs.writeFileSync('notes.json', dataJSON);
@@ -57,8 +65,8 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 };
