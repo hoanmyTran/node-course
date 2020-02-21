b3bd77418ec1976028facbca44a3cfb6c16408e6
hoan-my.tran <hoan-my.tran@horoquartz.fr>
2020-02-21 16:05:33 +0100

    static model

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,34 @@
+const path = require('path')
 const express = require('express');
 
+console.log(__dirname);
+console.log(path.join(__dirname, '../public'));
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (request, response) => {
     response.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (request, response) => {
-    response.send('Help page');
+    response.send({
+        name: 'My',
+        age: 27
+    });
 });
 
 app.get('/about', (request, response) => {
-    response.send('About');
+    response.send('<h1>About</h1>');
 });
 
 app.get('/weather', (request, response) => {
-    response.send('Your weather');
+    response.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    });
 });
 
 // app.com
